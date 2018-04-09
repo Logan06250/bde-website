87b27c43ba1f961cf350527d0b5a9919444d7ef8
Lamouar Logan <23254947+Logan06250@users.noreply.github.com>
2018-04-09 07:11:45 -0700

    feat : Authentification done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,31 +1,23 @@
-<!doctype html>
+@extends('layouts.app')
 
-<html lang="fr">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
 
-        <title> Site du BDE CESI NICE </title>
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="BDE Website">
-        <meta name="author" content="Logan Lamouar, Montet Bastien, Pierrini Eva, Le Rest Sylvain">
-
-        <!-- This website is using:  https://www.bootstrapcdn.com/ -->
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js" integrity="sha384-feJI7QwhOS+hwpX2zkaeJQjeiwlhOP+SdQDqhgvvo1DsjtiSQByFdThsxO669S2D" crossorigin="anonymous"></script>
-
-
-    </head>
-
-
-    <body>
-        @include('header');
-
-        <h1> un simple texte </h1>
-
-        @include('footer');
-
-    </body>
-</html>
-
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
