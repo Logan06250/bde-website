88d3367fbf7dc3c0c84ccd32d8f8c8d3aca661ce
Lamouar Logan <23254947+Logan06250@users.noreply.github.com>
2018-04-10 01:39:45 -0700

    feat : add menu
    the menu is added on the layout.app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,30 +18,107 @@
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
                 <div class="navbar-header">
+<!-- Navbar blade template start-->
+<nav class="navbar navbar-default navbar-inverse">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">HOME</a>
+        </div>
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="{{ (Request::is('/') ? 'active' : '') }}">
+                    <a href="{{ url('home') }}"><i class="fa fa-home"></i> Evènements</a>
+                </li>
+                <li class="{{ (Request::is('articles') ? 'active' : '') }}">
+                    <a href="{{ url('shop') }}">Boutique</a>
+                </li>
+                <li class="{{ (Request::is('about') ? 'active' : '') }}">
+                    <a href="{{ url('idea') }}">Boite à idées</a>
+                </li>
+                <li class="{{ (Request::is('contact') ? 'active' : '') }}">
+                    <a href="{{ url('contact') }}">Contacte ton BDE !</a>
+                </li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                @if (Auth::guest())
+                    <li class="{{ (Request::is('auth/login') ? 'active' : '') }}"><a href="{{ route('login') }}"><i
+                                    class="fa fa-sign-in"></i> Connexion</a></li>
+                    <li class="{{ (Request::is('auth/register') ? 'active' : '') }}"><a
+                    href="{{ route('register') }}">Inscription</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                           aria-expanded="false"><i class="fa fa-user"></i> {{"Bonjour "}}{{ Auth::user()->name }} <i
+                                    class="fa fa-caret-down"></i></a>
+                        <ul class="dropdown-menu" role="menu">
+                            @if(Auth::check())
+                                @if(Auth::user()->admin==1)
+                                    <li>
+                                        <a href="{{ url('admin/dashboard') }}"><i class="fa fa-tachometer"></i> Panel d'administration</a>
+                                    </li>
+                                @endif
+                                <li role="presentation" class="divider"></li>
+                            @endif
+                            
+                            <li>
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Déconexion
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                            
+                            <li>
+                                <a href="{{ url('help') }}"><i class="fa fa-sign-out"></i> Support d'aide</a>
+                            </li>
+                        </ul>
+                    </li>
+                @endif
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!-- Navbar blade template stop-->
+
+<!--                    
+<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
                         <span class="sr-only">Toggle Navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
 
-                    <!-- Branding Image -->
+                  
                     <a class="navbar-brand" href="{{ url('/') }}">
                         {{ config('app.name', 'Laravel') }}
                     </a>
+
+
                 </div>
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
+                   
                     <ul class="nav navbar-nav">
                         &nbsp;
                     </ul>
 
-                    <!-- Right Side Of Navbar -->
+                    
                     <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
+                        
                         @guest
                             <li><a href="{{ route('login') }}">Connexion</a></li>
                             <li><a href="{{ route('register') }}">Inscription</a></li>
@@ -66,7 +143,7 @@
                                 </ul>
                             </li>
                         @endguest
-                    </ul>
+                    </ul>-->
                 </div>
             </div>
         </nav>
