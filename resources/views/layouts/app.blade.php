074e0e273b96aa7caf68c68e910e773e8a5f9de1
Lamouar Logan <23254947+Logan06250@users.noreply.github.com>
2018-04-11 00:48:29 -0700

    feat : remove home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,12 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
@@ -18,89 +16,74 @@
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
                 <div class="navbar-header">
-<!-- Navbar blade template start-->
-<nav class="navbar navbar-default navbar-inverse">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle Navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">HOME</a>
-        </div>
-
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li class="{{ (Request::is('/') ? 'active' : '') }}">
-                    <a href="{{ url('event') }}"><i class="fa fa-home"></i> Evènements</a>
-                </li>
-                <li class="{{ (Request::is('articles') ? 'active' : '') }}">
-                    <a href="{{ url('shop') }}">Boutique</a>
-                </li>
-                <li class="{{ (Request::is('about') ? 'active' : '') }}">
-                    <a href="{{ url('idea') }}">Boite à idées</a>
-                </li>
-                <li class="{{ (Request::is('contact') ? 'active' : '') }}">
-                    <a href="{{ url('contact') }}">Contacte ton BDE !</a>
-                </li>
-            </ul>
-
-            <ul class="nav navbar-nav navbar-right">
-                @if (Auth::guest())
-                    <li class="{{ (Request::is('auth/login') ? 'active' : '') }}"><a href="{{ route('login') }}"><i
-                                    class="fa fa-sign-in"></i> Connexion</a></li>
-                    <li class="{{ (Request::is('auth/register') ? 'active' : '') }}"><a
-                    href="{{ route('register') }}">Inscription</a></li>
-                @else
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                           aria-expanded="false"><i class="fa fa-user"></i> {{"Bonjour "}}{{ Auth::user()->name }} <i
-                                    class="fa fa-caret-down"></i></a>
-                        <ul class="dropdown-menu" role="menu">
-                            @if(Auth::check())
-                                @if(Auth::user()->role==2)
-                                    <li>
-                                        <a href="{{ url('admin') }}"><i class="fa fa-tachometer"></i> Panel d administration</a>
+                    <!-- Navbar blade template start-->
+                    <nav class="navbar navbar-default navbar-inverse">
+                        <div class="container">
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                                        data-target="#bs-example-navbar-collapse-1">
+                                    <span class="sr-only">Toggle Navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                                <a class="navbar-brand" href="{{ url('/') }}">BDE</a>
+                            </div>
+                            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                                <ul class="nav navbar-nav">
+                                    <li class="{{ (Request::is('events') ? 'active' : '') }}">
+                                        <a href="{{ url('events') }}"><i class="fa fa-home"></i> Evènements</a>
                                     </li>
-                                @endif
-                                <li role="presentation" class="divider"></li>
-                            @endif
-                            
-                            <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Déconexion
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
+                                    <li class="{{ (Request::is('articles') ? 'active' : '') }}">
+                                        <a href="{{ url('articles') }}">Boutique</a>
                                     </li>
-                            
-                            <li>
-                                <a href="{{ url('help') }}"><i class="fa fa-sign-out"></i> Support d'aide</a>
-                            </li>
-                        </ul>
-                    </li>
-                @endif
-            </ul>
-        </div>
-    </div>
-</nav>
-
-<!-- Navbar blade template stop-->
+                                    <li class="{{ (Request::is('ideas') ? 'active' : '') }}">
+                                        <a href="{{ url('ideas') }}">Boite à idées</a>
+                                    </li>
+                                    <li class="{{ (Request::is('contact') ? 'active' : '') }}">
+                                        <a href="{{ url('contact') }}">Contacte ton BDE !</a>
+                                    </li>
+                                </ul>
+                                <ul class="nav navbar-nav navbar-right">
+                                    @if (Auth::guest())
+                                        <li class="{{ (Request::is('auth/login') ? 'active' : '') }}"><a href="{{ route('login') }}"><i
+                                                        class="fa fa-sign-in"></i> Connexion</a></li>
+                                        <li class="{{ (Request::is('auth/register') ? 'active' : '') }}"><a
+                                        href="{{ route('register') }}">Inscription</a></li>
+                                    @else
+                                        <li class="dropdown">
+                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                                                aria-expanded="false"><i class="fa fa-user"></i> {{"Bonjour "}}{{ Auth::user()->name }} <i
+                                                        class="fa fa-caret-down"></i></a>
+                                            <ul class="dropdown-menu" role="menu">
+                                                @if(Auth::check())
+                                                    @if(Auth::user()->role==2)
+                                                        <li>
+                                                            <a href="{{ url('admin') }}"><i class="fa fa-tachometer"></i> Panel d administration</a>
+                                                        </li>
+                                                        <li role="presentation" class="divider"></li>
+                                                    @endif
+                                                    <li>
+                                                        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                                            Déconnexion
+                                                        </a>
+                                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                            {{ csrf_field() }}
+                                                        </form>
+                                                    </li>
+                                                @endif
+                                            </ul>
+                                        </li>
+                                    @endif
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
                 </div>
             </div>
         </nav>
-
         @yield('content')
     </div>
-
-    <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
