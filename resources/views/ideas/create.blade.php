de59e21e669b9fa126de534c45788fc9f1ec7128
Le Rest Sylvain <23254878+baelishSir@users.noreply.github.com>
2018-04-11 01:04:53 -0700

    feat: add ideas functionality

---
+++ resources/views/ideas/create.blade.php
@@ -0,0 +1,43 @@
+<!-- create.blade.php -->
+
+@extends('layouts.app')
+@section('content')
+  @if (Route::has('login'))
+    @auth
+      <div class="container">
+        <h2>Merci de prendre le temps de créer une idée</h2><br/>
+        <form method="post" action="{{url('ideas')}}" enctype="multipart/form-data">
+          {{ csrf_field() }}
+          <div class="row">
+            <div class="col-md-4"></div>
+           <div class="form-group col-md-4">
+              <label for="Creator">Cette idée va être créer au nom de :</label>
+              <input type="text" class="form-control" name="creator" value="{{Auth::user()->name}}" readonly="readonly">
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="Title">Titre :</label>
+              <input type="text" class="form-control" name="title">
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="Description">Description :</label>
+              <input type="text" class="form-control" name="description">
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-4"></div>
+            <div class="form-group col-md-4" style="margin-top:60px">
+              <button type="submit" class="btn btn-success">Submit</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    @else
+    @endauth
+  @endif
+@endsection
