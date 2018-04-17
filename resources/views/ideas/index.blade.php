77a153cb83ab9c9faa9376ce7bb755558d357ef7
Le Rest Sylvain <23254878+baelishSir@users.noreply.github.com>
2018-04-17 02:44:18 -0700

    fix: ideas pages appearances

--- resources/views/ideas/index.blade.php
+++ resources/views/ideas/index.blade.php
@@ -6,22 +6,25 @@
     @endif
     <div class="row">
       @foreach($ideas as $idea)
-        <div class="panel panel default" style="maxheight:300px;">
-          <div class="panel-heading">
-            <a href="{{action('IdeaController@view',$idea['id'])}}">
-            <h1 class="panel-title">{{$idea['title']}}</h1></a>
-            <p>Vote total
-              <!-- {{$nbVote = 0}} -->
-              @foreach($votes as $vote)
-                @if($vote['idea_id'] == $idea['id'])
-                  <!-- {{$nbVote++}} -->
-                @endif
-              @endforeach
-              {{$nbVote}}
-            </p>
-          </div>
-          <div class="panel-body">
-            {{$idea['description']}}
+        <div class="col-md-4">
+          <div class="panel panel default" style="maxheight:300px;">
+           <div class="panel-heading">
+              <a href="{{action('IdeaController@view',$idea['id'])}}">
+              <h1 class="panel-title">{{$idea['title']}}</h1></a>
+              <p>Vote total
+                <!-- {{$nbVote = 0}} -->
+                @foreach($votes as $vote)
+                  @if($vote['idea_id'] == $idea['id'])
+                    <!-- {{$nbVote++}} -->
+                  @endif
+                @endforeach
+                {{$nbVote}}
+              </p>
+            </div>
+            <hr />
+            <div class="panel-body">
+              {{$idea['description']}}
+            </div>
           </div>
         </div>
       @endforeach
--- resources/views/ideas/view.blade.php
+++ resources/views/ideas/view.blade.php
@@ -1,83 +1,68 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container"><br />
-    @if (\Session::has('success'))
-      <div class="alert alert-success">
-        <p>{{ \Session::get('success') }}</p>
-      </div><br />
-    @endif
-    <table class="table table-striped">
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>Titre</th>
-          <th>Createur</th>
-          <th>Description</th>
-          <th>Nb vote</th>
-          <th colspan="4">Action</th>
-        </tr>
-      </thead>
-        <tbody>  
-          @foreach($ideas as $idea)
-            <tr>
-              <td>{{$idea['id']}}</td>
-              <td>{{$idea['title']}}</td>
-              <td>{{$idea['creator']}}</td>
-              <td>{{$idea['description']}}</td>  
-              <td>
-              <!-- {{$nbVote = 0}} -->
-              @foreach($votes as $vote)
-                @if($vote['idea_id'] == $idea['id'])
-                  <!-- {{$nbVote++}} -->
-                @endif
-              @endforeach
-              {{$nbVote}}
-              </td>
-              @if(Auth::check())
-              
-              <td>
-             
-                {{$voted=false}}
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-8">
+            <h1>{{$idea['title']}}</h1>
+            <h5>{{$idea['creator']}}</h5>
+            <h5>
+                Nombre de vote : 
+                <!-- {{$nbVote=0}} -->
                 @foreach($votes as $vote)
-                  @if($vote['idea_id'] == $idea['id'] && $vote['user_id'] == Auth::user()->id)
-                    <form  action="{{action('VoteController@destroy', $vote['id'])}}" method="post">
-                      {{ csrf_field() }}
-                      <input name="_method" type="hidden" value="DELETE">
-                      <button TYPE="submit" class="btn btn-danger">Dévoter</button>
-                    </form>
-                    <!--{{$voted=true}}-->
-                    @break 
-                   @endif
-                @endforeach
-                  @if($voted==false)
-                      <form method="post" action="{{url('votes')}}" enctype="multipart/form-data">
-                      {{ csrf_field() }}
-                      <input value ="{{$idea['id']}}" type="hidden" class="form-control" name="idea_id">
-                      <input value ="{{Auth::user()->id}}" type="hidden" class="form-control" name="user_id">
-                      <button TYPE="submit" class="btn btn-info">Voter</button>
-                    </form>
-                    
+                    @if($vote['idea_id']==$idea['id'])
+                    <!-- scan the votes table to increment and display the total number of vote -->
+                        <!-- {{$nbVote++}} -->
                     @endif
-                  @endif
-                  </td>
-                  @if(Auth::user()->isAdmin() || Auth::user()->isBDE())
-              
-                <td><a href="{{action('IdeaController@edit', $idea['id'])}}" class="btn btn-warning">Edit</a></td>
-                <td>
-                  <form action="{{action('IdeaController@destroy', $idea['id'])}}" method="post">
-                    {{ csrf_field() }}
-                    <input name="_method" type="hidden" value="DELETE">
-                    <button class="btn btn-danger" type="submit">Delete</button>
-                  </form>
-                </td>
-                <td>
-                <a href="{{action('IdeaController@ideaEvent', $idea['id'])}}" class="btn btn-primary">Event</a>
-                </td>
-              @endif
-            </tr>
-          @endforeach
-        </tbody>
-    </table>
-      <a href="{{action('IdeaController@create')}}" class="btn btn-primary">Proposer une idée</a>
+                @endforeach
+                {{$nbVote}}
+            </h5></br>
+        </div>
+        <div class="col-xs-6 col-md-4">
+            <div class="row">
+                <div class="col-xs-6 col-sm-4">
+                    @if(Auth::check())
+                    <!-- check if the uses is connected and display buttons if it's true -->
+                        {{$voted=false}}
+                        @foreach($votes as $vote)
+                        <!-- use a loop to scan the votes table -->
+                            @if($vote['idea_id'] == $idea['id'] && $vote['user_id'] == Auth::user()->id)
+                            <!-- verify if the connected user already vote for the idea. If it's true, display the un-vote button -->
+                                <form method="post" action="{{action('VoteController@destroy', $vote['id'])}}">
+                                    {{ csrf_field() }}
+                                    <input name="_method" type="hidden" value="DELETE">
+                                    <button class="btn btn-danger" type="submit">Dévoter</button>
+                                </form>
+                                <!-- {{$voted=true}} -->
+                                @break
+                            @endif
+                        @endforeach
+                        @if($voted==false)
+                        <!-- if the connected user haven't voted yet, display the vote button -->
+                            <form method="post" action="{{url('votes')}}" enctype="multipart/form-data">
+                                {{ csrf_field() }}
+                                <input value="{{$idea['id']}}" type="hidden" class="form-control" name="idea_id">
+                                <input value="{{Auth::user()->id}}" type="hidden" class="form-control" name="user_id">
+                                <button class="btn btn-primary" type="submit">Voter</button>
+                            </form>
+                        @endif
+                </div>
+                <div class="col-xs-6 col-sm-4">
+                @if(Auth::user()->isAdmin() || Auth::user()->isBDE())
+                    <a href="{{action('IdeaController@edit', $idea['id'])}}" class="btn btn-warning">Modifier</a>
+                </div>
+                <div class="clearfix visible-xs-block"></div>
+                <div class="col-xs-6 col-sm-4">
+                    <form action="{{action('IdeaController@destroy', $idea['id'])}}" method="post">
+                        {{ csrf_field() }}
+                        <input name="_method" type="hidden" value="DELETE">
+                        <button class="btn btn-danger" type="submit">Supprimer</button>
+                    </form>
+                    @endif
+                @endif
+                </div>
+            </div>
+        </div>
     </div>
+    {{$idea['description']}}
+</div>
   @endsection
