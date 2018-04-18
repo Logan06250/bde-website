5d7d9b4e1cd66841e295a92e658570e54447b8de
Le Rest Sylvain <23254878+baelishSir@users.noreply.github.com>
2018-04-17 23:38:40 -0700

    fix: button forget on pages idea

--- resources/views/ideas/view.blade.php
+++ resources/views/ideas/view.blade.php
@@ -10,7 +10,6 @@
                 <!-- {{$nbVote=0}} -->
                 @foreach($votes as $vote)
                     @if($vote['idea_id']==$idea['id'])
-                    <!-- scan the votes table to increment and display the total number of vote -->
                         <!-- {{$nbVote++}} -->
                     @endif
                 @endforeach
@@ -21,30 +20,6 @@
             <div class="row">
                 <div class="col-xs-6 col-sm-4">
                     @if(Auth::check())
-                    <!-- check if the uses is connected and display buttons if it's true -->
-                        {{$voted=false}}
-                        @foreach($votes as $vote)
-                        <!-- use a loop to scan the votes table -->
-                            @if($vote['idea_id'] == $idea['id'] && $vote['user_id'] == Auth::user()->id)
-                            <!-- verify if the connected user already vote for the idea. If it's true, display the un-vote button -->
-                                <form method="post" action="{{action('VoteController@destroy', $vote['id'])}}">
-                                    {{ csrf_field() }}
-                                    <input name="_method" type="hidden" value="DELETE">
-                                    <button class="btn btn-danger" type="submit">Dévoter</button>
-                                </form>
-                                <!-- {{$voted=true}} -->
-                                @break
-                            @endif
-                        @endforeach
-                        @if($voted==false)
-                        <!-- if the connected user haven't voted yet, display the vote button -->
-                            <form method="post" action="{{url('votes')}}" enctype="multipart/form-data">
-                                {{ csrf_field() }}
-                                <input value="{{$idea['id']}}" type="hidden" class="form-control" name="idea_id">
-                                <input value="{{Auth::user()->id}}" type="hidden" class="form-control" name="user_id">
-                                <button class="btn btn-primary" type="submit">Voter</button>
-                            </form>
-                        @endif
                 </div>
                 <div class="col-xs-6 col-sm-4">
                 @if(Auth::user()->isAdmin() || Auth::user()->isBDE())
@@ -52,6 +27,14 @@
                 </div>
                 <div class="clearfix visible-xs-block"></div>
                 <div class="col-xs-6 col-sm-4">
+                    @if($idea['visibility'])
+                    <a href="{{action('IdeaController@private',$idea['id'])}}" class="btn btn-warning">Signaler</a>
+                    @else
+                    <a href="{{action('IdeaController@unPrivate',$idea['id'])}}" class="btn btn-warning">Remettre</a>
+                    @endif
+                    <a href="{{action('IdeaController@ideaEvent', $idea['id'])}}" class="btn btn-primary">Event</a>
+                </div>
+                <div class="col-xs-6 col-sm-4"> 
                     <form action="{{action('IdeaController@destroy', $idea['id'])}}" method="post">
                         {{ csrf_field() }}
                         <input name="_method" type="hidden" value="DELETE">
